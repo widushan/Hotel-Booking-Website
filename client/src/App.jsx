1e187e824499c5e0f519fc1f37b31f0029ee2860
widushan <114930601+widushan@users.noreply.github.com>
2025-07-16 14:21:40 +0530

    all_rooms commit
    All Rooms page Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Footer from './components/Footer.jsx';
+import AllRooms from './pages/AllRooms.jsx';
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
         </Routes>
       </div>
 
--- client/src/components/HotelCard.jsx
+++ client/src/components/HotelCard.jsx
@@ -22,7 +22,7 @@ const HotelCard = ({room, index}) => {
                 <span>{room.hotel.address}</span>
             </div>
             <div className='flex items-center justify-between mt-4'>
-                <p><span className='text-xl text-gray-800'>${room.pricePerNight}/night</span></p>
+                <p><span className='text-xl text-gray-800'>Rs.{room.pricePerNight}/night</span></p>
                 <button className='px-4 py-2 text-sm font-medium border border-gray-300 rounded hover:bg-gray-50 transition-all cursor-pointer'>Book Now</button>
             </div>
         </div>
