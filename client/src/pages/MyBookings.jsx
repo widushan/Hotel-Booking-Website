b49cc1e313332bfb6abc847689454d751ebe9eb7
widushan <114930601+widushan@users.noreply.github.com>
2025-07-17 10:02:56 +0530

    booking commit
    booking draft page designed

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -65,6 +65,11 @@ const MyBookings = () => {
                             {booking.isPaid ? "Paid" : "Unpaid"}
                             </p>
                         </div>
+
+                        {!booking.isPaid && (
+                            <button className='px-4 py-1.5 mt-4 text-xs border border-grey-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer'>Pay Now</button>
+                        )}
+
                     </div>
 
 
